ab616c81a77319b71babe6a8e4f602f5c68a14d9
GhermanCristian <gherman_cristi10@yahoo.com>
2020-04-14 23:51:42 +0300

    Lab 3 - Timid implementation steps

--- Lab 3/Matrix.cpp
+++ Lab 3/Matrix.cpp
@@ -2,19 +2,59 @@
 #include <exception>
 using namespace std;
 
+void Matrix::insertElement(int line, int column, TElem value){
+	if (value == 0) {
+		removeElement(line, column);
+		return;
+	}
+	
+	int position = getPositionLargerThan(value);
+	if (position == -1) {
+		; // we add the element to the end of the list
+	}
+	else {
+		;
+	}
+
+	this->nrElements++;
+}
+
+void Matrix::removeElement(int line, int column){
+	this->nrElements--;
+}
+
+int Matrix::getPositionLargerThan(TElem value){
+	int currentPosition;
+	for (currentPosition = this->head; currentPosition != -1 and value > elements[currentPosition].getValue(); currentPosition = nextPosition[currentPosition]);
+
+	return currentPosition; // if -1 => the element should be appended to the list (there is no "larger")
+}
+
 Matrix::Matrix(int nrLines, int nrCols) {
-	   
-	//TODO - Implementation
+	this->numberOfLines = nrLines;
+	this->numberOfColumns = nrCols;
+
+	// initialising the SLLA
+	this->capacity = INITIAL_CAPACITY;
+	this->elements = new MatrixElement[this->capacity];
+	this->nextPosition = new int[this->capacity];
+
+	this->firstEmpty = 0; // list is empty
+	for (int i = 0; i < this->capacity - 1; i++) {
+		this->nextPosition[i] = i + 1;
+	}
+	this->nextPosition[this->capacity - 1] = -1;
+
+	this->head = -1; // list is empty => there is no head
+	this->nrElements = 0;
 }
 
 int Matrix::nrLines() const {
-	//TODO - Implementation
-	return 0;
+	return this->numberOfLines;
 }
 
 int Matrix::nrColumns() const {
-	//TODO - Implementation
-	return 0;
+	return this->numberOfColumns;
 }
 
 TElem Matrix::element(int i, int j) const {
@@ -25,6 +65,12 @@ TElem Matrix::element(int i, int j) const {
 TElem Matrix::modify(int i, int j, TElem e) {
 	//TODO - Implementation
 	return NULL_TELEM;
+}
+
+Matrix::~Matrix(){
+	// SLLA destructor
+	delete[] this->elements;
+	delete[] this->nextPosition;
 }
 
 
--- Lab 3/Matrix.h
+++ Lab 3/Matrix.h
@@ -1,12 +1,25 @@
 #pragma once
+#include "matrixElement.h"
 
 //DO NOT CHANGE THIS PART
-typedef int TElem;
 #define NULL_TELEM 0
+#define INITIAL_CAPACITY 100
 
 class Matrix {
 	private:
-		//TODO - Representation
+		MatrixElement* elements;
+		int* nextPosition;
+		int capacity;
+		int head;
+		int firstEmpty;
+		int nrElements;
+
+		int numberOfLines;
+		int numberOfColumns;
+
+		void insertElement(int line, int column, TElem value);
+		void removeElement(int line, int column); // happens when we add sth with value == 0
+		int getPositionLargerThan(TElem value);
 
 	public:
 		//constructor
@@ -26,4 +39,6 @@ class Matrix {
 		//returns the previous value from the position
 		//throws exception if (i,j) is not a valid position in the Matrix
 		TElem modify(int i, int j, TElem e);
+
+		~Matrix();
 };
